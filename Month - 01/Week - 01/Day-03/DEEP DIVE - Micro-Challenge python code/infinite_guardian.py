56b82d0bdbeb7b7af90cb890fe683ccb3d7021d8
Sojib Roy <rcsojib.cse1@gmail.com>
2025-12-17 19:04:41 +0600

    Add files via upload

---
+++ Month - 01/Week - 01/Day-03/DEEP DIVE - Micro-Challenge python code/infinite_guardian.py
@@ -0,0 +1,6 @@
+while True:
+    password = input("Enter password(type 'stop' to exit): ")
+    if password == 'stop':
+        print("program stopped")
+        break
+    print("password recieved, try Again")
