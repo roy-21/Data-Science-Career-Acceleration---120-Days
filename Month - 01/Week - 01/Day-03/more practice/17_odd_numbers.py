3a47fcf1cfc07eabcb1973b63128a00893316f34
Sojib Roy <rcsojib.cse1@gmail.com>
2025-12-27 22:37:38 +0600

    Add files via upload

---
+++ Month - 01/Week - 01/Day-03/more practice/17_odd_numbers.py
@@ -0,0 +1,4 @@
+# Print odd numbers
+for i in range(1, 21):
+    if i % 2 != 0:
+        print(i)
---
+++ Month - 01/Week - 01/Day-03/more practice/18_sum_list.py
@@ -0,0 +1,6 @@
+# Sum of list elements
+nums = [1, 2, 3, 4, 5]
+total = 0
+for n in nums:
+    total += n
+print("Sum:", total)
