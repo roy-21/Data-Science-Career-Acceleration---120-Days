4845943faf2e21a868f4375b1238cf1138176b63
Sojib Roy <rcsojib.cse1@gmail.com>
2026-01-04 00:42:31 +0600

    Add files via upload

---
+++ Month - 01/Week - 01/Day-04/more practice/01_list_creation_access.py
@@ -0,0 +1,8 @@
+# List creation and element access
+students = ["Rahim", "Karim", "Ayesha", "Sumi"]
+
+print("First student:", students[0])
+print("Last student:", students[-1])
+
+for i in range(len(students)):
+    print("Index", i, "=>", students[i])
